9439306aae26e09ea40a49b85785f25f5cb464f1
Lennart <117754915+TyrionGER@users.noreply.github.com>
2024-03-21 11:55:12 +0100

    Add files via upload

---
+++ DasBlinkenlights/Debug/blink.d
@@ -0,0 +1,24 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h
+blink.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/msp430fr2355.h
+blink.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h
+blink.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+blink.obj: C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+blink.obj: C:/Program\ Files/ccs/ccs_base/msp430/include/legacy.h
+
+../blink.c:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/in430.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/Program\ Files/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Program\ Files/ccs/ccs_base/msp430/include/legacy.h:
+
